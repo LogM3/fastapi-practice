0432d99ca81d4eb9c6d35b98de613c72e746b7c8
Max L <maksutor@yandex.ru>
2025-10-16 15:27:09 +0300

    Added db check endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from typing import Any
-from fastapi import FastAPI, HTTPException, Request
+from typing import Annotated, Any
+from fastapi import Depends, FastAPI, HTTPException, Request
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.dependencies import get_db_connection
 from app.core.exceptions import TokenExpiredError
 from app.users.handlers import router as user_router
 from app.auth.handlers import router as auth_router
@@ -19,6 +22,18 @@ app.include_router(test_router)
 @app.get('/')
 async def root():
     return {'status': 'ok'}
+
+
+@app.get('/db_check')
+async def check_db(
+    session: Annotated[AsyncSession, Depends(get_db_connection)]
+) -> dict[str, str]:
+    try:
+        result = (
+            await session.execute(text('SELECT version();'))).scalar_one()
+        return {'version': result}
+    except ConnectionRefusedError:
+        raise HTTPException(500, 'Database unavailable')
 
 
 @app.middleware('http')
